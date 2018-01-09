abae3d956931b144fdb3887ca54dfd1422df6e28
Håkan Wieslander <hakan.wieslander@it.uu.se>
2018-01-09 13:54:10 +0000

    Added to image_analysis.py

--- haste_processing_node/image_analysis/image_analysis.py
+++ haste_processing_node/image_analysis/image_analysis.py
@@ -1,9 +1,13 @@
-
+from PIL import Image
+import io
+import numpy as np
 
 def extract_image_features(metadata, image_bytes):
     # TODO extract image features here
+    image = Image.open(io.BytesIO(image_bytes))
+    sum = np.sum(image)
     extracted_features = {
         'image_height': 123,
-        'number_of_green_pixels': 42
+        'number_of_green_pixels': sum
     }
     return extracted_features
