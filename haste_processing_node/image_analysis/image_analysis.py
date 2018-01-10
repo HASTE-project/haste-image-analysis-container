d530b8aae5cce60ce9e2e457aeaebd73b4769281
Hakanwie <32453742+Hakanwie@users.noreply.github.com>
2018-01-10 11:35:12 +0100

    Add files via upload
    Updated image analysis code to extract features from the green color channel

--- haste_processing_node/image_analysis/image_analysis.py
+++ haste_processing_node/image_analysis/image_analysis.py
@@ -1,14 +1,33 @@
 from PIL import Image
 import io
+import numpy as np
+from skimage.feature import greycomatrix, greycoprops
+from skimage.filters import laplace
+
+
+def laplaceVariance(im):
+    lap_var = laplace(im).var()
+    return lap_var
+
+
+def corr(im):
+    # Needs a 2D image
+    if len(im.shape) > 2:
+        print('Only works with 2D images')
+        return 0
+
+    glcm = greycomatrix(im.astype('uint8'), [1], [0], normed=True)
+    stats = greycoprops(glcm, 'correlation')
+    return np.mean(stats)
 
 
 def extract_image_features(metadata, image_bytes):
-    # TODO extract image features here
 
-    image = Image.open(io.BytesIO(image_bytes))
+    image = np.array(Image.open(io.BytesIO(image_bytes)))
 
     extracted_features = {
-        'image_height': 123,
-        'number_of_green_pixels': 42
+        'sum_of_intensities': np.sum(image),
+        'correlation': corr(image),
+        'laplaceVariance': laplaceVariance(image)
     }
     return extracted_features
