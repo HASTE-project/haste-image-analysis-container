6e0a19011491ae9003112b1746f4bc38368c69d3
Ben Blamey <blamey.ben@gmail.com>
2019-01-30 10:39:34 +0100

    fix tests

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,5 +1,5 @@
 import pytest
-from haste_processing_node.function import process_data, extract_image_features
+from haste_processing_node.function import process_data, extract_features
 import pickle
 import time
 
@@ -29,6 +29,5 @@ def test_extract_image_features():
     image_bytes = bytes(fh.read())
     fh.close()
 
-    extracted_features = extract_image_features({'location': (12.34, 56.78),
-                                                 'image_length_bytes': len(image_bytes)}, image_bytes)
+    extracted_features = extract_features(image_bytes)
     print(extracted_features)
